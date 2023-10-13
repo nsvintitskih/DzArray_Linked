abf6d23ecf4b85a3854e4f81628ae67acbb26cfb
nsvintitskih <nsvintitskih@yandex.ru>
2023-10-13 23:19:56 +0300

    DzArray_Linked

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        for (int i = 0; i < 500000; i++) {
+            arrayList.add(i);
+            linkedList.add(i);
+        }
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < arrayList.size(); i++)
+            arrayList.get(i);
+        long end = System.currentTimeMillis();
+        System.out.println("Time for ArrayList " +(end-start) + "ms");
+
+         start = System.currentTimeMillis();
+        for (int i = 0; i < linkedList.size(); i++)
+            linkedList.get(i);
+         end = System.currentTimeMillis();
+        System.out.println("Time for LinkedList " +(end-start) + "ms");
+
+
     }
 }
